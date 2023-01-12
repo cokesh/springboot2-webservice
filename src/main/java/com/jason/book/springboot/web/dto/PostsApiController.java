e112456235e67909ae83e72f390ac1f133c4c50f
cokesh <spdlqjlwjs@daum.net>
2023-01-12 17:48:01 +0900

    추가 controller, Dto

---
+++ src/main/java/com/jason/book/springboot/web/dto/PostsApiController.java
@@ -0,0 +1,19 @@
+package com.jason.book.springboot.web.dto;
+
+import com.jason.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PutMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+}
